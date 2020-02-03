1c1739c5b0feed892be46fdbb456e99a7bb760eb
Michael Kuperfish Steinberg <36902556+Michael-K-Stein@users.noreply.github.com>
2020-02-03 17:51:14 +0200

    Create Ares2.asm

---
+++ Bots/Final - Ares/Code/Ares2.asm
@@ -0,0 +1,68 @@
+@start:
+mov si, ax
+add ax,@call_far-@start
+
+push es
+push ss
+pop es
+mov dx, sp
+add si, @CopyMe
+mov cl, 0Dh
+mov cx, (@EndCopyMe - @CopyMe + 1)/2
+rep movsw
+mov sp, dx
+pop es
+
+push ds
+push es
+pop ds
+mov dx, [0x0]
+add dx, 0x0300
+mov dl, 0xA3
+pop ds
+
+push ds
+push ss
+pop ds
+mov bx,40h
+mov [bx+2],cs
+mov [bx],dx
+pop ds
+
+mov ax, 0x1FFF
+push ss
+push ds
+pop es
+mov di, dx
+stosw
+pop ds
+
+push cs
+pop ss
+
+add dx, 0x0300
+
+mov sp,dx
+
+dec di
+
+xor si, si
+
+mov cl, (@EndCopyMe - @CopyMe + 1)/2
+
+@call_far:
+call far [bx]
+
+@CopyMe:
+rep movsw
+sub sp, (0x05E8 + (@EndCopyMe - @CopyMe - 1))
+mov cl, (@EndCopyMe - @CopyMe + 1)/2
+mov ax, 0x1FFF
+sub di, (0x0900 + (@EndCopyMe - @CopyMe) + 4)
+mov dx, di
+mov [bx], di
+stosw
+xor si, si
+dec di
+jmp dx
+@EndCopyMe:
