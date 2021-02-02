fa334bb8d01e1da97b2ea58f7dbdf34c23c56c81
Michael Kuperfish Steinberg <36902556+Michael-K-Stein@users.noreply.github.com>
2021-02-02 10:03:41 +0200

    Create GreenSlate1.asm

---
+++ 2021/Bots/Alternating_Call_Far_Rep/GreenSlate1.asm
@@ -0,0 +1,112 @@
+CS_OFFSET equ 0x01
+COCAINE_LINES equ 2
+GAP equ 0x0F00
+
+@start:
+
+;Copy the recusive thing into the extra segment
+mov cx, (@END_Area_To_Copy-@Area_To_Copy)/2
+mov si, ax
+add si, @Area_To_Copy
+rep movsw
+
+MOV dx, cs
+add dx, CS_OFFSET
+push dx
+pop ds
+
+
+PUSH dx
+mov dx,ax
+add dx, @AfterCSFuckery-(CS_OFFSET*16)
+PUSH dx
+
+; Change AX so that we don't hit the first line
+and ax, 0xF000
+add ax, GAP
+
+
+mov bh, ah
+mov word [bx - GAP +0x03 + 0xA0], 0x1FFF
+mov word [bx - GAP +0x01 + 0xA0], 0xEC29
+
+RETF
+
+@AfterCSFuckery:
+mov al, 0xA1
+sub ax, GAP
+
+push ds
+push es
+pop ds
+
+mov bx,50h
+mov [bx+2],cs
+mov [bx],ax
+
+pop es
+
+push cs
+pop ss
+
+mov sp,ax
+add sp, GAP
+
+xor si,si
+mov di,ax
+inc di
+mov cx, (@END_Area_To_Copy_mid-@Area_To_Copy)/2
+
+mov ax, 0x1FFF
+mov bp, 0x24
+
+jmp [bx]
+
+@call_far:
+sub sp, bp
+call far [bx]
+
+
+
+@end:
+
+
+@Area_To_Copy:
+
+REP movsw
+sub di, (@END_Area_To_Copy_mid-@Area_To_Copy) +3 + (GAP*3) - 0x0600
+mov word [bx], di
+sub si,2
+movsw
+movsw
+sub di,3
+mov cl, (@END_Area_To_Copy_mid-@Area_To_Copy)/2
+dec bp
+jmp [bx]
+nop
+nop
+nop
+
+@END_Area_To_Copy_mid:
+
+sub sp, bp
+call far [bx]
+
+REP movsw
+sub di, (@END_Area_To_Copy_mid-@Area_To_Copy) +3 + GAP
+mov word [bx], di
+sub si,2
+movsw
+movsw
+sub di,3
+mov cl, (@END_Area_To_Copy_mid-@Area_To_Copy)/2
+xor si,si
+inc bp
+jmp [bx]
+nop
+
+
+sub sp, bp
+call far [bx]
+
+@END_Area_To_Copy:
