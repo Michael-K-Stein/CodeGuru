23384e2dae44b08812cb86a694483dd4ef30cd53
Michael Kuperfish Steinberg <36902556+Michael-K-Stein@users.noreply.github.com>
2021-02-02 19:20:09 +0200

    Update GreenSlate2.asm

--- 2021/Bots/Alternating/Ver_Stack/Alternating3/GreenSlate2.asm
+++ 2021/Bots/Alternating/Ver_Stack/Alternating3/GreenSlate2.asm
@@ -25,12 +25,12 @@ add dx, @AfterCSFuckery-(CS_OFFSET*16)
 PUSH dx
 
 ; Change AX so that we don't hit the first line
-and ax, 0x4000
+and ax, 0xc000
 add ax, GAP + (GAP*3)-0x0600 - 0x0100
 
 
 mov bh, ah
-mov word [bx - GAP +0x03 + 0xA0], 0x1FFF
+mov word [bx - GAP +0x03 + 0xA0], 0x18FF
 mov word [bx - GAP +0x01 + 0xA0], 0xEC29
 
 RETF
@@ -60,7 +60,7 @@ mov di,ax
 inc di
 mov cx, (@END_Area_To_Copy1_mid-@Area_To_Copy1)/2
 
-mov ax, 0x1FFF
+;mov ax, 0x1FFF
 mov bp, 0x24
 
 jmp [bx]
@@ -143,6 +143,6 @@ nop
 @END_Area_To_Copy4_mid:
 
 sub sp,bp
-call far [bx]
+call far [bx+si]
 
 @END_Area_To_Copy:
